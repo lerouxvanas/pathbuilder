5c0673216866a62508af1501890036233fa46378
Leroux van As <lerouxvanas@yahoo.com>
2023-10-16 16:37:51 +0200

    added jwt

--- src/index.php
+++ src/index.php
@@ -1,11 +1,48 @@
 <?php
 
+declare(strict_types=1);
+namespace MyApp;
+require 'vendor/autoload.php';
+
+use DateTimeImmutable;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\JwtFacade;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Signer\Key\InMemory;
+
+use function var_dump;
+
+$key = InMemory::base64Encoded(
+    'hiG8DlOKvtih6AxlZn5XKImZ06yu8I3mkOzaJrEuW8yAv8Jnkw330uMt8AEqQ5LB'
+);
+
+$token = (new JwtFacade())->issue(
+    new Sha256(),
+    $key,
+    static fn (
+        Builder $builder,
+        DateTimeImmutable $issuedAt
+    ): Builder => $builder
+        ->issuedBy('https://api.my-awesome-app.io')
+        ->permittedFor('https://client-app.io')
+        ->expiresAt($issuedAt->modify('+10 minutes'))
+);
+
+echo "<pre>";
+var_dump($token->claims()->all());
+echo "</pre>";
+echo "<hr />";
+echo $token->toString();
+
 $request = $_SERVER['REQUEST_URI'];
 $viewDir = '/views/';
 
 switch ($request) {
     case '':
     case '/':
+        require __DIR__ . $viewDir . 'home.php';
+        break;
+    case '/home':
         require __DIR__ . $viewDir . 'home.php';
         break;
 
